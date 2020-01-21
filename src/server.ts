3450f1dd0d7d9dff5e246fd0ea054c8fd071eb24
Kevin Laburte <kevin.laburte@gmail.com>
2020-01-21 11:10:25 -0500

    set up boilerplate

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+// tslint:disable: import-name
+
+// load .env data into process.env
+import { config } from 'dotenv';
+config();
+
+import express from 'express';
+import path from 'path';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import DB from './lib/db';
+import { dbParams } from './lib/config-vars';
+
+// server config
+const ENV = process.env.ENV || 'development';
+const PORT = parseInt(process.env.PORT || '8080', 10);
+
+// instantiate db and storage
+const db = new DB(dbParams);
+
+const app = express();
+
+// use morgan in dev only
+(ENV === 'development') && app.use(morgan('dev'));
+
+// register middlewares
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+
+// hello world
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// serve static files in public/
+app.use(express.static(path.join(__dirname, 'public')));
+
+// start listening
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
