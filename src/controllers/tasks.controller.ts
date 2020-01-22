ee278ca1e6ae77f801bd1c08d0268e8450254e50
Kevin Laburte <kevin.laburte@gmail.com>
2020-01-21 20:24:35 -0500

    Add default reminder to task post route

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import DB from '../lib/db';
 import taskModel from '../models/tasks.model';
+import reminderModel from '../models/reminders.model';
 
 export default function(db: DB) {
 
@@ -23,8 +24,27 @@ export default function(db: DB) {
     // create new task
     .post(async (req, res) => {
       try {
-        const task = await taskModel.createTask(db.query, req.body);
-        // create default reminder
+
+        const task = await db.transaction(async (query) => {
+
+          const task = await taskModel.createTask(query, req.body);
+
+          // set up default reminder if due in more than 5 mins
+          const fiveMinutes = 1000 * 60 * 5;
+          if (task.dueAt - Date.now() > fiveMinutes) {
+            const reminderInput = {
+              taskId: task.id,
+              notes: `Your task ${task.name} is almost due.`,
+              dueAt: task.dueAt - fiveMinutes
+            }
+
+            await reminderModel.createReminder(query, reminderInput);
+          }
+
+          return task;
+
+        })
+        // const task = await taskModel.createTask(db.query, req.body);
 
         res.json(task);
 
